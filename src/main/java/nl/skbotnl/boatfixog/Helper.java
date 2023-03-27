f9a519f9bfc61557a805c76c8cbfc95899dd079c
SKBotNL <68018116+SKBotNL@users.noreply.github.com>
2023-03-27 08:58:08 +0200

    Initial commit

---
+++ src/main/java/nl/skbotnl/boatfixog/Helper.java
@@ -0,0 +1,25 @@
+package nl.skbotnl.boatfixog;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Helper {
+    public static Map<UUID, Long> boatTimeout = new HashMap<>();
+
+    public static long getBoatTimeout(UUID uuid) {
+        long cooldown;
+        if (!boatTimeout.containsKey(uuid)) {
+            boatTimeout.put(uuid, System.currentTimeMillis());
+        }
+
+        if (System.currentTimeMillis() - boatTimeout.get(uuid) > 150) {
+            boatTimeout.put(uuid, System.currentTimeMillis());
+            cooldown = 0;
+        }
+        else {
+            cooldown = System.currentTimeMillis() - boatTimeout.get(uuid);
+        }
+        return cooldown;
+    }
+}
---
+++ src/main/java/nl/skbotnl/boatfixog/MoveBoatRunnable.java
@@ -0,0 +1,22 @@
+package nl.skbotnl.boatfixog;
+
+import net.minecraft.world.entity.vehicle.EntityBoat;
+import net.minecraft.world.phys.Vec3D;
+import net.minecraft.world.entity.EnumMoveType;
+import org.bukkit.util.Vector;
+
+public class MoveBoatRunnable implements Runnable {
+    private EntityBoat boat;
+    private Vector vec;
+
+    public MoveBoatRunnable(EntityBoat boat, Vector vec) {
+        this.boat = boat;
+        this.vec = vec;
+    }
+
+    @Override
+    public void run() {
+        // move(EnumMoveType arg0, Vec3D arg1)
+        boat.a(EnumMoveType.a, new Vec3D(vec.getX(), vec.getY(), vec.getZ()));
+    }
+}
