2678f181fee67c15abf6cae778ef498fd343c033
NotAlexNoyle <alexnoyle@icloud.com>
2024-11-01 04:56:16 +0000

    Update gradle, fix build

---
+++ build.gradle.kts
@@ -0,0 +1,104 @@
+plugins {
+    id("com.gradleup.shadow") version "8.3.2" // Import shadow API.
+    java // Tell gradle this is a java project.
+    eclipse // Import eclipse plugin for IDE integration.
+    kotlin("jvm") version "2.0.21" // Import kotlin jvm plugin for kotlin/java integration.
+}
+
+java {
+    // Declare java version.
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+group = "nl.skbotnl.rewindfixog" // Declare bundle identifier.
+version = "1.2.1" // Declare plugin version (will be in .jar).
+val apiVersion = "1.19" // Declare minecraft server target version.
+
+tasks.named<ProcessResources>("processResources") {
+    val props = mapOf(
+        "version" to version,
+        "apiVersion" to apiVersion
+    )
+
+    inputs.properties(props) // Indicates to rerun if version changes.
+
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+    maven {
+        url = uri("https://repo.purpurmc.org/snapshots/")
+    }
+    maven {
+        url = uri("https://repo.viaversion.com/")
+    }
+    maven {
+        url = uri("https://repo.dmulloy2.net/repository/public/")
+    }
+	maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+	maven {
+		url = uri("https://oss.sonatype.org/content/repositories/snapshots") // Spigot dependency.
+	}
+	maven {
+		url = uri("https://oss.sonatype.org/content/repositories/central") // Spigot dependency.
+	}
+	maven {
+        url = uri("file://${System.getProperty("user.home")}/.m2/repository") // Import BuildTools from MavenLocal.
+    }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot:1.19.4-R0.1-SNAPSHOT")
+	compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3") // Import MiniPlaceholders API.
+    compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT") // Import Purpur API.
+    compileOnly("com.viaversion:viaversion-api:5.0.5") // Import ViaVersion API.
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0") // Import ProtocolLib API.
+
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach { // Ensure reproducible builds.
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("") // Use empty string instead of null
+    from("LICENSE") {
+        into("/")
+    }
+    exclude("io.github.miniplaceholders.*") // Exclude the MiniPlaceholders package from being shadowed.
+    minimize()
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.jar {
+    archiveClassifier.set("part")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+    options.compilerArgs.add("-Xlint:deprecation") // Triggers deprecation warning messages.
+    options.encoding = "UTF-8"
+    options.isFork = true
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+        vendor = JvmVendorSpec.GRAAL_VM
+    }
+}
